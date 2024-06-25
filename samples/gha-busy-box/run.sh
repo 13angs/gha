325d3c4e37f5476b26d76f0de6a6ae2e8d77e952
13angs <13angz.um@gmail.com>
2024-06-25 03:59:43 +0000

    add gha-busy-box proj

---
+++ samples/gha-busy-box/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Hello, World!"
+echo "This is a BusyBox container."
+echo "BusyBox combines tiny versions of many common UNIX utilities into a single small executable."
+echo "For example, here are some details about the current system:"
+echo "Hostname: $(hostname)"
+echo "Date: $(date)"
+echo "Uptime: $(uptime)"
